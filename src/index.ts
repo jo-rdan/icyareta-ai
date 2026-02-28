96e1ad7e03671323a81b91d565fa6745f109cdce
jo-Rdan250 <51836855+jo-rdan@users.noreply.github.com>
2026-02-28 05:01:01 +0200

    setup base server

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Icyareta Backend Active",
+    status: "Ready for NESA Simulations",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
