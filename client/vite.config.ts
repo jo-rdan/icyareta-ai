156e5695caa7faecac7223f13a5739423065c32a
jo-Rdan250 <51836855+jo-rdan@users.noreply.github.com>
2026-04-12 14:12:14 +0200

    change favicon

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,6 +21,12 @@ export default defineConfig({
         scope: "/",
         start_url: "/",
         icons: [
+          {
+            src: "fav_icon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
           { src: "pwa-192x192.png", sizes: "192x192", type: "image/png" },
           { src: "pwa-512x512.png", sizes: "512x512", type: "image/png" },
           {
